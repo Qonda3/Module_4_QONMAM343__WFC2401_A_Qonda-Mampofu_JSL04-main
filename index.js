a3e591d3f1c3a554f50d20af9fc22bd65e603034
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-03-22 11:33:15 +0200

    Update late-night drink variable

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ console.log(tempStatus);
 
 // Define the current hour in 24-hour format
 let currentHour = 22; 
-
+let drinkOpt = currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.';
+console.log(drinkOpt);
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
