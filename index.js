6959ac650d0a1cbfcfc871fae89ed088653d7271
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-03-25 10:27:45 +0200

    correct syntax error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 let userPreference = 'Sugar-free';// Possible values: 'Regular', 'Sugar-free', or undefined
 let preferredFlavor = userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(`${flavorPreference}`);
+console.log(`${preferredPreference}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
